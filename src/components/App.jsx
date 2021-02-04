3ee9f46f23f46818b37252831bcfb25352fd3455
Dmitrii Martynov <dmitriimartynov1@gmail.com>
2021-02-04 13:51:38 +0300

    working on images

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import '../styles/index.scss'
+import sword from '../images/swc-sword.png'
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
           <section>
             <h1>Oh hai, React</h1>
           </section>
+          <img src={sword} alt='sword' width='250' />
         </main>
       </section>
     </>
